98e25d04c5fd4541a66cae07fe8043ba5605676a
David Jackson <david@dwjackson.ca>
2013-10-06 13:20:34 -0400

    Fix rule parser to allow certain rules
    Specifically, rules that have control characters as their "variable"
    (thse should always be "constant" rules). An example is (+ -> +).

--- express/models/rule_parser.js
+++ express/models/rule_parser.js
@@ -9,6 +9,9 @@ function RuleParser(str) {
     var rule_str;
     var variable;
 
+    var variable_regex = /[A-Z]/;
+    var control_regex = /[\+-\[\]]/;
+
     for (var i = 0; i < this.str.length; i++) {
       c = this.str[i];
       if (i > 0) {
@@ -20,13 +23,14 @@ function RuleParser(str) {
       if (c == '(') {
 	env = 1;
 	rule_str = "";
-      } else if ((c.search(/[A-Z]/) == 0 || c.search(/[\+\-\[\]]/) == 0)
+      } else if (c == '>' && d == '-') {
+	env = 3;
+      } else if ((c.search(variable_regex) == 0 || c.search(control_regex) == 0)
                  && env == 1) {
 	variable = c;
-      } else if (c == '>' && d == '-') {
 	env = 2;
-      } else if ((c.search(/[A-Z]/) == 0 || c.search(/[\+-\[\]]/) == 0)
-                 && env == 2) {
+      } else if ((c.search(variable_regex) == 0 || c.search(control_regex) == 0)
+                 && env == 3) {
 	rule_str += c;
       } else if (c == ')') {
 	this.rules[variable] = rule_str;
